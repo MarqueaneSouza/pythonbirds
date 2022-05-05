1b38d43c976fd2d43e45dbd978c3bf63224fada1
Marqueane <marqueane_64@hotmail.com>
2022-05-05 20:41:49 -0300

    Criado método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,9 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+if __name__ == '__main__': #crio a 'main' para nossos testes
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p)) #para executar o método eu peço o cumprimentar e passo o 'p'.
+    print(id(p))
+    print(p.cumprimentar()) #executo o método utilizando o próprio objeto. E nesse caso, eu não preciso passar o objeto novamente.
+
