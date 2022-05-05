225c55d8ce371f57b72af50d224dd7803923dab6
Marqueane <marqueane_64@hotmail.com>
2022-05-05 20:56:06 -0300

    Criado atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade = 35): #ALT + ENTER inclui o parâmetro (atributo) na linha abaixo do método.
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 if __name__ == '__main__': #crio a 'main' para nossos testes
-    p = Pessoa()
+    p = Pessoa('Luciano')
     print(Pessoa.cumprimentar(p)) #para executar o método eu peço o cumprimentar e passo o 'p'.
     print(id(p))
     print(p.cumprimentar()) #executo o método utilizando o próprio objeto. E nesse caso, eu não preciso passar o objeto novamente.
+    print(p.nome)
+    p.nome = "Renzo" #alterando o nome
+    print(p.nome)
 
