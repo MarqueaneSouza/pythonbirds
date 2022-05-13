7eebecb4d314a4dbe91dcfdb4c3b51ba4df2018a
Marqueane <marqueane_64@hotmail.com>
2022-05-12 21:05:53 -0300

    Criado atributo complexo 'filhos'

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,22 @@
 class Pessoa:
-    def __init__(self, nome=None, idade = 35): #ALT + ENTER inclui o parâmetro (atributo) na linha abaixo do método.
+    def __init__(self, *filhos, nome=None, idade = 35): #ALT + ENTER inclui o parâmetro (atributo) na linha abaixo do método.
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 if __name__ == '__main__': #crio a 'main' para nossos testes
-    p = Pessoa('Luciano')
-    print(Pessoa.cumprimentar(p)) #para executar o método eu peço o cumprimentar e passo o 'p'.
-    print(id(p))
-    print(p.cumprimentar()) #executo o método utilizando o próprio objeto. E nesse caso, eu não preciso passar o objeto novamente.
-    print(p.nome)
-    p.nome = "Renzo" #alterando o nome
-    print(p.nome)
+    marqueane = Pessoa(nome='Marqueane')
+    luciano = Pessoa(marqueane, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano)) #para executar o método eu peço o cumprimentar e passo o 'p'.
+    print(id(luciano))
+    print(luciano.cumprimentar()) #executo o método utilizando o próprio objeto. E nesse caso, eu não preciso passar o objeto novamente.
+    print(luciano.nome)
+    print(luciano.idade)
+
+    for filho in luciano.filhos:
+        print(filho.nome)
+
+
 
